13170626548f239c56d2344baa443c9d90d5bf8f
Sarah Allen <sarah@ultrasaurus.com>
2024-03-12 19:11:45 -0700

    refactor into 2 examples

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# html5evver examples
+
+* different simple example https://github.com/servo/html5ever/pull/504/files
+
 References
 * https://stackoverflow.com/questions/35654525/parsing-html-page-content-in-a-stream-with-hyper-and-html5ever
 * lychee crate
---
+++ examples/extract-style.rs
@@ -0,0 +1,80 @@
+use html5ever::{
+    Attribute,
+    buffer_queue::BufferQueue, tendril::{SliceExt, StrTendril},
+    tokenizer::{Tag, Token, TokenSink, TokenSinkResult, Tokenizer, TokenizerOpts},
+};
+use  string_cache::Atom;
+use markup5ever::LocalNameStaticSet;
+
+struct StyleExtractor {
+    styles: Vec<String>
+}
+impl StyleExtractor {
+    pub fn new() -> Self {
+        StyleExtractor {
+            styles: Vec::new()
+        }
+    }
+}
+
+fn find_attr_by_name<'a>(attrs: &'a Vec<Attribute>, name: &str) -> Option<&'a Attribute> {
+    attrs.iter().find(|&attr| &attr.name.local == name)
+}
+
+fn find_attr_by_namevalue<'a>(attrs: &'a Vec<Attribute>, name: &str, value: &str) -> Option<&'a Attribute> {
+    attrs.iter().find(|&attr|
+        &attr.name.local == name &&
+        attr.value == value.to_tendril()
+    )
+}
+
+impl TokenSink for StyleExtractor {
+    type Handle = ();
+
+    #[allow(clippy::match_same_arms)]
+    fn process_token(&mut self, token: Token, _line_number: u64) -> TokenSinkResult<()> {
+        let _link:Atom<LocalNameStaticSet> = "link".into();
+        match token {
+            Token::TagToken(
+                Tag{name: _link ,attrs,..}) => {
+                    if find_attr_by_namevalue(&attrs,
+                        "rel", "stylesheet").is_some() {
+                        if let Some(href_attr) = find_attr_by_name(&attrs, "href") {
+                            self.styles.push(href_attr.value.to_string())
+                        }
+                    }
+            },
+            _ => {
+
+            }
+        }
+        TokenSinkResult::Continue
+    }
+}
+fn main() {
+    println!("Hello, world!");
+    const HTML_INPUT: &str = r#"
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style/main.css">
+    </head>
+    <body>
+        <p>This is a paragraph</p>
+        <p><b>bold</b></p>
+    </body>
+</html>"#;
+
+
+    let mut input = BufferQueue::new();
+    input.push_back(StrTendril::from(HTML_INPUT));
+
+    let mut tokenizer = Tokenizer::new(
+    StyleExtractor::new(),
+    TokenizerOpts::default(),
+    );
+    let _handle = tokenizer.feed(&mut input);
+    tokenizer.end();
+
+    println!("{:?}",tokenizer.sink.styles);
+
+}
